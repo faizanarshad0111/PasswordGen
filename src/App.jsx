9e6755e89eb0e065defd0219baa9d7940b16742c
Faizan <faizanrana369@gmail.com>
2024-03-30 02:40:59 +0500

    Add more

--- src/App.jsx
+++ src/App.jsx
@@ -39,13 +39,13 @@ function App() {
   }, [length, numberAllowed, charAllowed, passwordGenetator]);
   return (
     <>
-      <div className="w-full max-w-full  shadow-md rounded-lg px-1 py-3 my-8 mx-1 bg-gray-800 text-orange-500">
+      <div className="w-max mx-auto shadow-md rounded-lg p-2 bg-gray-800 text-orange-500">    
         <h2 className="text-white text-center my-3">Password generator</h2>
-        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <div className="flex flex-wrap shadow rounded-lg overflow-hidden mb-4">
           <input
             type="text"
             value={password}
-            className="outline-none w-full py-1 px-3"
+            className="outline-none w-full py-1 px-3 mb-5"
             placeholder="Password"
             readOnly
             ref={passwordRef}
@@ -59,7 +59,7 @@ function App() {
             copy
           </button>
         </div>
-        <div className="flex text-sm gap-x-2">
+        <div className="flex flex-wrap text-sm gap-x-2">
           <div className="flex items-center gap-x-1">
             <input
               type="range"
